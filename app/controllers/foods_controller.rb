997a32a791b8eaa24ff70e0e654373d27edecf01
Isaac Pitwa <isaacpitwa256@gmail.com>
2022-10-04 19:07:56 +0300

    Add foods data for views

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,10 +1,15 @@
 class FoodsController < ApplicationController
   def new
+    @food = Food.new
   end
 
   def create
+    
   end
 
   def index
+    @foods = Food.all
   end
+
+  def destroy; end
 end
