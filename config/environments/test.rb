f05c33240dd26ccce06ef6e3f2d48b60bf121ae1
masangana <alexmasangana@gmail.com>
2022-10-04 15:21:17 +0300

    Add devise views

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -7,6 +7,11 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.after_initialize do
+    Bullet.enable        = true
+    Bullet.bullet_logger = true
+    Bullet.raise         = true # raise an error if n+1 query occurs
+  end
 
   # Turn false under Spring and add config.action_view.cache_template_loading = true.
   config.cache_classes = true
@@ -57,4 +62,5 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
