184db6fe472e96928f851597049bcac2de5b8201
masangana <alexmasangana@gmail.com>
2022-10-04 20:36:13 +0300

    Add edit food

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -27,6 +27,21 @@ class RecipeFoodsController < ApplicationController
     redirect_to user_recipe_url(@recipe.user, @recipe)
   end
 
+  def edit
+    @recipe_food = RecipeFood.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:recipe_id])
+    @recipe_food = @recipe.recipe_foods.find(params[:id])
+    if @recipe_food.update(recipe_food_params)
+      flash[:notice] = 'Food updated successfully.'
+      redirect_to user_recipe_url(@recipe.user, @recipe)
+    else
+      render 'edit'
+    end
+  end
+
   def recipe_food_params
     params.permit(:quantity, :food_id)
   end
