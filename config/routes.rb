a2b87c0da8abbbe77cf2e1df80d4a08e6111d10c
Isaac Pitwa <isaacpitwa256@gmail.com>
2022-10-06 00:03:37 +0300

    Style Public Recipes

--- config/routes.rb
+++ config/routes.rb
@@ -10,11 +10,12 @@ Rails.application.routes.draw do
 
   # Define routes for recipes
   resources :users do
+    resources :foods, only: [:index, :new, :create, :destroy]
     resources :recipes do
-      #resources :recipe_foods
-      get "/foods/:id/remove" => "recipes#remove_food", as: :remove_food
-      get "/foods/add" => "recipes#add_food_view", as: :add_food_view
-      post "/foods/add" => "recipes#add_food", as: :add_food
+      # #resources :recipe_foods
+      # get "/foods/:id/remove" => "recipes#remove_food", as: :remove_food
+      # get "/foods/add" => "recipes#add_food_view", as: :add_food_view
+      # post "/foods/add" => "recipes#add_food", as: :add_food
       resources :recipe_foods, only: [:new, :create, :destroy, :update, :edit]
     end
     resources :shoppings, only:[:index]
