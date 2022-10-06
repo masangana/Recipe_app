1b3ac331ed254faa92d6e781f6bc4856b4667d46
masangana <alexmasangana@gmail.com>
2022-10-06 14:12:02 +0300

    clean

--- app/models/food.rb
+++
@@ -1,6 +0,0 @@
-class Food < ApplicationRecord
-  belongs_to :user
-  has_many :recipes_foods, dependent: :destroy, class_name: 'RecipeFood'
-
-  validates :name, :quantity, :measurement_unit, :price, presence: true
-end
--- app/models/recipe.rb
+++
@@ -1,6 +0,0 @@
-class Recipe < ApplicationRecord
-  belongs_to :user
-  has_many :recipe_foods, dependent: :destroy, class_name: 'RecipeFood'
-
-  validates :name, :description, :preparation_time, :cooking_time, presence: true
-end
